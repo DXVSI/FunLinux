8bd6edc71d121f5729a1b195d5845c9f36045379
DXVSI <dxvsi123@gmail.com>
2025-11-02 03:46:04 +0800

    dnfish: исправлена установка нескольких пакетов одной командой
    - install теперь принимает несколько пакетов: install pkg1 pkg2 pkg3
    - Проверяются все пакеты перед установкой
    - Все пакеты устанавливаются одной командой dnf5
    - Улучшены сообщения для единичных и множественных пакетов

--- dnfish/install.fish
+++ dnfish/install.fish
@@ -5,39 +5,47 @@ function install --description 'dnf5 install (with cowsay/cowthink + lolcat)'
     end
 
     if test (count $argv) -eq 0
-        _say "Использование: install <имя_пакета>" warning
+        _say "Использование: install <имя_пакета> [пакет2 пакет3 ...]" warning
         return 1
     end
 
-    set -l NameArg $argv[1]
+    set -l packages $argv
 
-    # Проверяем существование пакета ПЕРЕД вопросом
-    _think "Проверяю пакет $NameArg..." info
+    # Проверяем существование пакетов ПЕРЕД вопросом
+    if test (count $packages) -eq 1
+        _think "Проверяю пакет $packages..." info
+    else
+        _think "Проверяю пакеты: $packages..." info
+    end
 
-    dnf5 info $NameArg &>/dev/null
-    set -l check_status $status
-
-    if test $check_status -ne 0
-        _say "Пакет $NameArg не найден в репозиториях!" dead
-        return 1
+    for pkg in $packages
+        dnf5 info $pkg &>/dev/null
+        if test $status -ne 0
+            _say "Пакет $pkg не найден в репозиториях!" dead
+            return 1
+        end
     end
 
     # Теперь спрашиваем об установке
-    _think "Установить $NameArg?"
+    if test (count $packages) -eq 1
+        _think "Установить $packages?"
+    else
+        _think "Установить пакеты: $packages?"
+    end
 
     while true
         read -l --prompt-str "Ответ [y/N]: " answer
         switch (string lower -- $answer)
             case y yes
-                _say "Ставлю $NameArg. Держись крепче!" info
-                sudo dnf5 install -y $NameArg
+                _say "Ставлю пакеты. Держись крепче!" info
+                sudo dnf5 install -y $packages
                 set -l install_status $status
                 if test $install_status -eq 0
-                    _say "Готово! $NameArg установлен." success
+                    _say "Готово! Пакеты установлены." success
                 else if test $install_status -eq 130
                     _say "Установка прервана (Ctrl+C)." dead
                 else
-                    _say "Упс… установка $NameArg не удалась (код ошибки: $install_status)." dead
+                    _say "Упс… установка не удалась (код ошибки: $install_status)." dead
                 end
                 break
             case '' n no
